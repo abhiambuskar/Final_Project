9e4f74dde81115c1d61c0bbbd6b405cde5b93696
Abhishek Ambuskar <87688149+abhiambuskar@users.noreply.github.com>
2023-10-13 14:20:10 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,7 @@
+// function test(){  
+//     let x = document.getElementById("name")
+//     console.log(x)
+//     let y = document.getElementById("product")
+//     console.log(y)
+//     y.innerHTML = " this is good"
+// }
